0b3b67cdc9bcd781ac7266a7527acdc48c5cd07a
Viktor Patraboi <vpatr@softserveinc.com>
2019-01-14 14:33:09 +0200

    added chord list. nedds logoc fix

--- src/chord.js
+++ src/chord.js
@@ -6,54 +6,28 @@ class SingleChord extends HTMLElement {
       canvasWidth: 390,
       canvasHeight: 140,
       maxRow: 12,
-      minRow: 0
+      minRow: 0,
+      reflect: true
     };
+    this.props = {};
 
-    this.props = {
-      id: "3",
-      name: "F",
-      startString: 2,
-      structure: {
-        strings: {
-          sixthString: [1],
-          fifthString: [1, 3],
-          fourthString: [1, 3],
-          thirdString: [1, 2],
-          secondStrind: [1],
-          firstString: [1]
-        }
-      }
-    };
+    this.idEnding = this.makeIdEnding();
 
-    this.chordId = `single-chord-${new Date().getTime()}`;
-    this.id = `single-chord-component-${new Date().getTime()}`;
-    this.rowNumberId = `row-number-${new Date().getTime()}`;
-    this.chordNameId = `chord-name-${new Date().getTime()}`;
-
-    this.innerHTML = `<div class="main-container">
-                            <div class="accord-description">
-                                <div id="${
-                                  this.chordNameId
-                                }" class="description-element">
-                                    ${this.props.name}
-                                </div>
-                                <div id="${
-                                  this.rowNumberId
-                                }" class="description-element row-number-container">
-                                    ${this.state.minRow ? this.state.minRow : 1}
-                                </div>
-                            </div>
-                            <canvas
-                                class="canvas-style"
-                                id=${this.chordId}
-                                width=${this.state.canvasWidth}
-                                height=${this.state.canvasHeight}/>
-                        </div>`;
+    this.chordId = `single-chord-${new Date().getTime()}-${this.idEnding}`;
+    this.id = `single-chord-component-${new Date().getTime()}-${this.idEnding}`;
+    this.rowNumberId = `row-number-${new Date().getTime()}-${this.idEnding}`;
+    this.chordNameId = `chord-name-${new Date().getTime()}-${this.idEnding}`;
+    this.innerHTML = "<div>Loading data</div>";
 
     setTimeout(() => {
-      //   this.drawChord();
-      this.initCanvas();
       this.chordComponentRef = document.getElementById(this.id);
+      this.initChord();
+      this.innerHTML = this.getCurrentTemplete();
+      this.initCanvas();
+
+      if (this.state.reflect) {
+        this.reflectChord();
+      }
       this.findBorders();
       this.calculateCanvasSize();
       setTimeout(() => {
@@ -66,6 +40,11 @@ class SingleChord extends HTMLElement {
         this.drawChord();
       });
     });
+  }
+
+  initChord() {
+    const chordName = this.chordComponentRef.getAttribute("chord");
+    this.props = chordList.find(el => el.name == chordName);
   }
 
   initCanvas() {
@@ -188,6 +167,366 @@ class SingleChord extends HTMLElement {
       15
     );
   }
+
+  reflectChord() {
+    this.state.ctx.scale(-1, 1);
+  }
+
+  getCurrentTemplete() {
+    const template = `<div class="main-container">
+                              <div class="accord-description">
+                                  <div id="${
+                                    this.chordNameId
+                                  }" class="description-element">
+                                      ${this.props.name}
+                                  </div>
+                                  <div id="${
+                                    this.rowNumberId
+                                  }" class="description-element row-number-container">
+                                      ${
+                                        this.state.minRow
+                                          ? this.state.minRow
+                                          : 1
+                                      }
+                                  </div>
+                              </div>
+                              <canvas
+                                  class="canvas-style"
+                                  id=${this.chordId}
+                                  width=${this.state.canvasWidth}
+                                  height=${this.state.canvasHeight}/>
+                          </div>`;
+    return template;
+  }
+
+  makeIdEnding() {
+    var text = "";
+    var possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  }
 }
+
+const chordList = [
+  {
+    id: "1",
+    name: "F",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [1],
+        fifthString: [1, 3],
+        fourthString: [1, 3],
+        thirdString: [1, 2],
+        secondStrind: [1],
+        firstString: [1]
+      }
+    }
+  },
+  {
+    id: "2",
+    name: "F7",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [1],
+        fifthString: [1, 3],
+        fourthString: [1],
+        thirdString: [1, 2],
+        secondStrind: [1],
+        firstString: [1]
+      }
+    }
+  },
+  {
+    id: "3",
+    name: "Fm",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [1],
+        fifthString: [1, 3],
+        fourthString: [1, 3],
+        thirdString: [1],
+        secondStrind: [1],
+        firstString: [1]
+      }
+    }
+  },
+  {
+    id: "4",
+    name: "C",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [],
+        fifthString: [3],
+        fourthString: [2],
+        thirdString: [],
+        secondStrind: [1],
+        firstString: []
+      }
+    }
+  },
+  {
+    id: "5",
+    name: "C7",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [],
+        fifthString: [3],
+        fourthString: [2],
+        thirdString: [3],
+        secondStrind: [1],
+        firstString: []
+      }
+    }
+  },
+  {
+    id: "6",
+    name: "Cm",
+    startString: 2,
+    structure: {
+      strings: {
+        sixthString: [2],
+        fifthString: [2],
+        fourthString: [2, 4],
+        thirdString: [2, 4],
+        secondStrind: [2, 3],
+        firstString: [2]
+      }
+    }
+  },
+  {
+    id: "7",
+    name: "D",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [],
+        fifthString: [],
+        fourthString: [],
+        thirdString: [2],
+        secondStrind: [3],
+        firstString: [2]
+      }
+    }
+  },
+  {
+    id: "8",
+    name: "D7",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [],
+        fifthString: [],
+        fourthString: [],
+        thirdString: [2],
+        secondStrind: [1],
+        firstString: [2]
+      }
+    }
+  },
+  {
+    id: "9",
+    name: "Dm",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [],
+        fifthString: [],
+        fourthString: [],
+        thirdString: [2],
+        secondStrind: [3],
+        firstString: [1]
+      }
+    }
+  },
+  {
+    id: "10",
+    name: "E",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [],
+        fifthString: [2],
+        fourthString: [2],
+        thirdString: [1],
+        secondStrind: [],
+        firstString: []
+      }
+    }
+  },
+  {
+    id: "11",
+    name: "E7",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [],
+        fifthString: [2],
+        fourthString: [],
+        thirdString: [1],
+        secondStrind: [],
+        firstString: []
+      }
+    }
+  },
+  {
+    id: "11",
+    name: "Em",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [],
+        fifthString: [2],
+        fourthString: [2],
+        thirdString: [],
+        secondStrind: [],
+        firstString: []
+      }
+    }
+  },
+  {
+    id: "12",
+    name: "G",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [3],
+        fifthString: [2],
+        fourthString: [],
+        thirdString: [],
+        secondStrind: [],
+        firstString: [3]
+      }
+    }
+  },
+  {
+    id: "13",
+    name: "G7",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [3],
+        fifthString: [2],
+        fourthString: [],
+        thirdString: [],
+        secondStrind: [],
+        firstString: [1]
+      }
+    }
+  },
+  {
+    id: "14",
+    name: "Gm",
+    startString: 2,
+    structure: {
+      strings: {
+        sixthString: [2, 5],
+        fifthString: [2, 5],
+        fourthString: [2],
+        thirdString: [2],
+        secondStrind: [2],
+        firstString: [2]
+      }
+    }
+  },
+  {
+    id: "15",
+    name: "A",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [],
+        fifthString: [],
+        fourthString: [2],
+        thirdString: [2],
+        secondStrind: [2],
+        firstString: []
+      }
+    }
+  },
+  {
+    id: "16",
+    name: "A7",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [],
+        fifthString: [],
+        fourthString: [2],
+        thirdString: [],
+        secondStrind: [2],
+        firstString: []
+      }
+    }
+  },
+  {
+    id: "17",
+    name: "Am",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [],
+        fifthString: [],
+        fourthString: [2],
+        thirdString: [2],
+        secondStrind: [1],
+        firstString: []
+      }
+    }
+  },
+  {
+    id: "18",
+    name: "B",
+    startString: 2,
+    structure: {
+      strings: {
+        sixthString: [2],
+        fifthString: [2],
+        fourthString: [2, 4],
+        thirdString: [2, 4],
+        secondStrind: [2, 4],
+        firstString: [2]
+      }
+    }
+  },
+  {
+    id: "19",
+    name: "Bm",
+    startString: 2,
+    structure: {
+      strings: {
+        sixthString: [2],
+        fifthString: [2],
+        fourthString: [2, 4],
+        thirdString: [2, 4],
+        secondStrind: [2, 3],
+        firstString: [2]
+      }
+    }
+  },
+  {
+    id: "20",
+    name: "B7",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [],
+        fifthString: [2],
+        fourthString: [1],
+        thirdString: [2],
+        secondStrind: [],
+        firstString: [2]
+      }
+    }
+  }
+];
 
 customElements.define("single-chord", SingleChord);
---
+++ src/constants.js
@@ -0,0 +1,48 @@
+export const chordList = [
+  {
+    id: "3",
+    name: "F",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [1],
+        fifthString: [1, 3],
+        fourthString: [1, 3],
+        thirdString: [1, 2],
+        secondStrind: [1],
+        firstString: [1]
+      }
+    }
+  },
+  {
+    id: "3",
+    name: "F7",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [1],
+        fifthString: [1],
+        fourthString: [1, 2],
+        thirdString: [1],
+        secondStrind: [1,3],
+        firstString: [1]
+      }
+    }
+  },
+  {
+    id: "3",
+    name: "Fm",
+    startString: 1,
+    structure: {
+      strings: {
+        sixthString: [1],
+        fifthString: [1],
+        fourthString: [1],
+        thirdString: [1,3],
+        secondStrind: [1,3],
+        firstString: [1]
+      }
+    }
+  },
+
+];
